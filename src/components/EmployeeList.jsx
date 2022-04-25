afc86bd66bc4d568359d437f48c6309b9e70ab8c
SVickyPawar <97332040+SVickyPawar@users.noreply.github.com>
2022-04-25 18:49:19 +0530

    Initiated

---
+++ src/components/EmployeeList.jsx
@@ -0,0 +1,13 @@
+export const EmployeeList = () => {
+    return (
+      <div className="list_container">
+        {/* On clicking this card anywhere, user goes to user details */}
+        <h1>me</h1>
+        <div className="employee_card">
+          <img className="employee_image" />
+          <span className="employee_name"></span>
+          <span className="employee_title"></span>
+        </div>
+      </div>
+    );
+  };
---
+++ src/components/Logout.jsx
@@ -0,0 +1,7 @@
+export const Logout=()=>{
+    return(
+        <div>
+            <h1>See ya</h1>
+        </div>
+    );
+}
